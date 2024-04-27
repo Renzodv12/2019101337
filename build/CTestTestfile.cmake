2889565f997a69921b81cfaafc3ffa62fef2e734
unknown <renzodragotto50@gmail.com>
2024-04-27 08:57:37 -0400

    commit iniciar

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Renzo_Dragotto/src
+# Build directory: C:/Renzo_Dragotto/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/catkin_generated/setup_cached.bat
@@ -0,0 +1,16 @@
+@echo off
+REM generated from catkin/python/catkin/environment_cache.py
+
+REM based on a snapshot of the environment before and after calling the setup script
+REM it emulates the modifications of the setup script without recurring computations
+
+REM new environment variables
+set LD_LIBRARY_PATH=c:\opt\ros\noetic\x64\lib
+set PYTHONHOME=C:\opt\ros\noetic\x64\
+
+REM modified environment variables
+set CMAKE_PREFIX_PATH=C:\opt\ros\noetic\x64\tools\vcpkg\installed\x64-windows;C:/Renzo_Dragotto/devel;c:\opt\ros\noetic\x64
+set PATH=C:\opt\ros\noetic\x64\tools\vcpkg\installed\x64-windows\bin;C:\opt\ros\noetic\x64\tools\vcpkg;c:\opt\ros\noetic\x64\bin;c:\opt\ros\noetic\x64\lib;C:\opt\ros\noetic\x64\;C:\opt\ros\noetic\x64\Scripts;c:\opt\ros\noetic\x64\Scripts;c:\opt\ros\noetic\x64\;C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.39.33519\bin\HostX64\x64;C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\IDE\VC\VCPackages;C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\IDE\CommonExtensions\Microsoft\TestWindow;C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\MSBuild\Current\bin\Roslyn;C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8 Tools\x64\;C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\\x64;C:\Program Files (x86)\Windows Kits\10\bin\\x64;C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\\MSBuild\Current\Bin\amd64;C:\Windows\Microsoft.NET\Framework64\v4.0.30319;C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\IDE\;C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\;C:\Program Files\Microsoft\jdk-17.0.10.7-hotspot\bin;C:\Program Files\Microsoft\jdk-11.0.20.8-hotspot\bin;C:\Program Files (x86)\VMware\VMware Workstation\bin\;C:\Program Files (x86)\Common Files\Oracle\Java\javapath;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Windows\System32\OpenSSH\;C:\Program Files\Microsoft SQL Server\150\Tools\Binn\;C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\170\Tools\Binn\;C:\Program Files\dotnet\;C:\Program Files (x86)\Microsoft SQL Server\150\Tools\Binn\;C:\Program Files\Microsoft SQL Server\150\DTS\Binn\;C:\Program Files (x86)\Microsoft SQL Server\150\DTS\Binn\;C:\Program Files\Azure Data Studio\bin;C:\Program Files\Docker\Docker\resources\bin;C:\ProgramData\chocolatey\bin;C:\Program Files\nodejs\;C:\Program Files (x86)\Windows Kits\10\Windows Performance Toolkit\;C:\Program Files\Git\cmd;C:\Users\renzo\AppData\Local\Microsoft\WindowsApps;C:\Users\renzo\AppData\Local\Programs\Microsoft VS Code\bin;C:\Users\renzo\.dotnet\tools;C:\Users\renzo\AppData\Roaming\npm;C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin;C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\IDE\CommonExtensions\Microsoft\CMake\Ninja;C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\IDE\VC\Linux\bin\ConnectionManagerExe
+set ROS_ETC_DIR=c:\opt\ros\noetic\x64/etc/ros
+set ROS_PACKAGE_PATH=C:/Renzo_Dragotto/src;%ROS_PACKAGE_PATH%
+set ROS_ROOT=c:\opt\ros\noetic\x64/share/ros
